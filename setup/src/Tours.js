5ec82ec9ad5c7889e532d0acab191c4babe16705
upneet2213 <upneet2213@gmail.com>
2021-08-02 21:17:34 +0530

    My Commit

---
+++ setup/src/Tours.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Tour from './Tour';
+const Tours = ({tours, removeTour}) => {
+  return <>
+  <div className="title">
+    <h2>Our Tours</h2>
+    <div className="underline"></div>
+  </div>
+  <div>
+    {tours.map((tour)=>{
+      return <Tour key={tour.id} {...tour} removeTour={removeTour}/>
+    })}
+  </div>
+  </>;
+};
+
+export default Tours;
